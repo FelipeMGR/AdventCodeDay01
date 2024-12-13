64ae2a0c08bc929be3825dca30511a3bc2bae823
Felipe Matheus <felipematheusgr18@gmail.com>
2024-12-13 10:57:24 -0300

    Adicionar arquivos de projeto.

---
+++ AdventCodeDay01/Converter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventCodeDay01
+{
+    internal class Converter
+    {
+        public List<int> convertToInt(string[] array)
+        {
+
+            List<int> convertedArray = new List<int>();
+            string pattern = @"\d+";
+
+            foreach (string line in array)
+            {
+                MatchCollection matches = Regex.Matches(line, pattern);
+                foreach (Match match in matches)
+                {
+                    if (int.TryParse(match.Value, out int number))
+                    {
+                        convertedArray.Add(number);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Conversão não realizada");
+                    }
+                }
+            }
+            return convertedArray;
+        }
+
+        public List<int> calculoDistancia(List<int> list)
+        {
+            List<int> calculatedValue = new List<int>();
+
+            for (int i = 0; i < list.Count - 1;i++)
+            {
+                int distance;
+                int value1 = list[i];
+                int value2 = list[i + 1];
+                if (value1 > value2)
+                {
+                    distance = value1 - value2;
+                    calculatedValue.Add(distance);
+                }
+                else
+                {
+                    distance = value2 - value1;
+                }
+            }
+            return calculatedValue;
+        }
+
+        public int distanceSum(List<int> list)
+        {
+            int sum = 0;
+            foreach(int value in list)
+            {
+                sum += value;
+            }
+
+            return sum;
+        }
+
+    }
+}
---
+++ AdventCodeDay01/Program.cs
@@ -0,0 +1,24 @@
+﻿using AdventCodeDay01;
+using System;
+
+namespace AdventoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter cv = new Converter();
+            string path = "C:\\Users\\felip\\OneDrive\\Documents\\advent01.txt";
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string[] content = sr.ReadToEnd().Split('\n', ' ');
+                List<int> converted = cv.convertToInt(content);
+                List<int> distance = cv.calculoDistancia(converted);
+
+                int distanceSum = cv.distanceSum(distance);
+                Console.WriteLine($"The total distance is: {distanceSum}");
+            }
+        }
+    }
+}
